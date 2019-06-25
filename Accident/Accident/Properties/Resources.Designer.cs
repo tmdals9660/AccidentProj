e7283aebc7ed5822ddec7bc7358e92e0be938e3e
상현 손 <sang30651@naver.com>
2019-06-25 14:47:11 +0900

    .

--- Accident/Accident/Properties/Resources.Designer.cs
+++ Accident/Accident/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace Accident.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        internal static System.Drawing.Bitmap SignUp {
+            get {
+                object obj = ResourceManager.GetObject("SignUp", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         internal static System.Drawing.Bitmap Statistics {
             get {
                 object obj = ResourceManager.GetObject("Statistics", resourceCulture);
