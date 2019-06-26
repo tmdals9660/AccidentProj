08e4354a48478c668ce114e1aee5eb83b4fadcb9
상현 손 <sang30651@naver.com>
2019-06-26 09:19:58 +0900

    .

--- Accident/Accident/From/Admin/Login.Designer.cs
+++ Accident/Accident/From/Admin/Login.Designer.cs
@@ -28,87 +28,87 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.btnLogin = new System.Windows.Forms.Button();
-            this.txbPw = new System.Windows.Forms.TextBox();
-            this.txbId = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // btnLogin
-            // 
-            this.btnLogin.Location = new System.Drawing.Point(105, 98);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(78, 23);
-            this.btnLogin.TabIndex = 2;
-            this.btnLogin.Text = "Login(&L)";
-            this.btnLogin.UseVisualStyleBackColor = true;
-            this.btnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
-            // 
-            // txbPw
-            // 
-            this.txbPw.Location = new System.Drawing.Point(21, 63);
-            this.txbPw.Name = "txbPw";
-            this.txbPw.PasswordChar = '*';
-            this.txbPw.Size = new System.Drawing.Size(247, 21);
-            this.txbPw.TabIndex = 1;
-            // 
-            // txbId
-            // 
-            this.txbId.Location = new System.Drawing.Point(21, 24);
-            this.txbId.Name = "txbId";
-            this.txbId.Size = new System.Drawing.Size(247, 21);
-            this.txbId.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 12);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "ID";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 48);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(23, 12);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "PW";
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.Location = new System.Drawing.Point(189, 98);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(79, 23);
-            this.btnCancel.TabIndex = 4;
-            this.btnCancel.Text = "Cancel(&C)";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
-            // 
-            // Login
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 132);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txbId);
-            this.Controls.Add(this.txbPw);
-            this.Controls.Add(this.btnLogin);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(300, 170);
-            this.Name = "Login";
-            this.Text = "r";
-            this.Load += new System.EventHandler(this.Login_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.btnLogin = new System.Windows.Forms.Button();
+			this.txbPw = new System.Windows.Forms.TextBox();
+			this.txbId = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnLogin
+			// 
+			this.btnLogin.Location = new System.Drawing.Point(105, 98);
+			this.btnLogin.Name = "btnLogin";
+			this.btnLogin.Size = new System.Drawing.Size(78, 23);
+			this.btnLogin.TabIndex = 2;
+			this.btnLogin.Text = "Login(&L)";
+			this.btnLogin.UseVisualStyleBackColor = true;
+			this.btnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+			// 
+			// txbPw
+			// 
+			this.txbPw.Location = new System.Drawing.Point(21, 63);
+			this.txbPw.Name = "txbPw";
+			this.txbPw.PasswordChar = '*';
+			this.txbPw.Size = new System.Drawing.Size(247, 21);
+			this.txbPw.TabIndex = 1;
+			// 
+			// txbId
+			// 
+			this.txbId.Location = new System.Drawing.Point(21, 24);
+			this.txbId.Name = "txbId";
+			this.txbId.Size = new System.Drawing.Size(247, 21);
+			this.txbId.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(19, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(16, 12);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "ID";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(19, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(23, 12);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "PW";
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Location = new System.Drawing.Point(189, 98);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(79, 23);
+			this.btnCancel.TabIndex = 4;
+			this.btnCancel.Text = "Cancel(&C)";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+			// 
+			// Login
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 132);
+			this.Controls.Add(this.btnCancel);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txbId);
+			this.Controls.Add(this.txbPw);
+			this.Controls.Add(this.btnLogin);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(300, 170);
+			this.Name = "Login";
+			this.Text = "Login";
+			this.Load += new System.EventHandler(this.Login_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
